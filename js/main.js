06fc5459812ec0cf2b68aa257b5a996d0f6c4849
Guilherme Barbosa <111584900+guilherme-barbosa-rodrigues@users.noreply.github.com>
2023-01-08 20:08:21 -0300

    up

--- js/main.js
+++ js/main.js
@@ -1,3 +1,54 @@
-function criptografar() {
-    const saida = document.querySelector('#resultado-area')
+const pesquisando = document.getElementById("pesquisando");
+const resultado = document.getElementById("resultado");
+
+function cripto() {
+    const saida = document.querySelector('#resultado-area');
+
+    pesquisando.style.display = "none";
+    resultado.style.display = "flex";
+
+    var entrada = document.querySelector("#texto-principal");
+    var mensagem = entrada.value;
+    var mensagemTexto = "";
+
+    var listaDeCaracteres = mensagem.split("")
+
+    for(var i = 0; i < listaDeCaracteres.lenght; i++) {
+        if(listaDeCaracteres[i] == "a"){
+            listaDeCaracteres[i] = "ai"
+        }
+        else if(listaDeCaracteres[i] == "e"){
+            listaDeCaracteres[i] = "enter"
+        }
+        else if(listaDeCaracteres[i] == "i"){
+            listaDeCaracteres[i] = "imes"
+        }
+        else if(listaDeCaracteres[i] == "o"){
+            listaDeCaracteres[i] = "ober"
+        }
+        else if(listaDeCaracteres[i] == "u"){
+            listaDeCaracteres[i] = "ufat"
+        }
+        mensagemTexto = mensagemTexto + listaDeCaracteres[i]
+    }
+    saida.value = mensagemTexto
+}
+
+function descripto() {
+    const saida = document.querySelector('#resultado-area');
+
+    pesquisando.style.display = "none";
+    resultado.style.display = "flex";
+
+    var entrada = document.querySelector("#texto-principal");
+    var mensagem = entrada.value;
+
+    mensagem = mensagem.replaceAll("ai", "a");
+    mensagem = mensagem.replaceAll("enter", "e");
+    mensagem = mensagem.replaceAll("imes", "i");
+    mensagem = mensagem.replaceAll("ober", "o");
+    mensagem = mensagem.replaceAll("ufat", "u")
+
+    saida.value = mensagem;
+    
 }
