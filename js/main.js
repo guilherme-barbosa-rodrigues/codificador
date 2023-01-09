9764e8a0a3c744a65fe6663122ea5c0dc09146a5
Guilherme Barbosa <111584900+guilherme-barbosa-rodrigues@users.noreply.github.com>
2023-01-09 00:14:49 -0300

    create primeira parte

--- js/main.js
+++ js/main.js
@@ -1,54 +1,41 @@
 const pesquisando = document.getElementById("pesquisando");
-const resultado = document.getElementById("resultado");
+const resultado =  document.getElementById("resultado");
 
 function cripto() {
-    const saida = document.querySelector('#resultado-area');
+    var output = document.querySelector('#texto-resultado');
 
     pesquisando.style.display = "none";
     resultado.style.display = "flex";
-
-    var entrada = document.querySelector("#texto-principal");
-    var mensagem = entrada.value;
-    var mensagemTexto = "";
-
-    var listaDeCaracteres = mensagem.split("")
-
-    for(var i = 0; i < listaDeCaracteres.lenght; i++) {
-        if(listaDeCaracteres[i] == "a"){
-            listaDeCaracteres[i] = "ai"
-        }
-        else if(listaDeCaracteres[i] == "e"){
-            listaDeCaracteres[i] = "enter"
-        }
-        else if(listaDeCaracteres[i] == "i"){
-            listaDeCaracteres[i] = "imes"
-        }
-        else if(listaDeCaracteres[i] == "o"){
-            listaDeCaracteres[i] = "ober"
-        }
-        else if(listaDeCaracteres[i] == "u"){
-            listaDeCaracteres[i] = "ufat"
-        }
-        mensagemTexto = mensagemTexto + listaDeCaracteres[i]
-    }
-    saida.value = mensagemTexto
-}
-
-function descripto() {
-    const saida = document.querySelector('#resultado-area');
-
-    pesquisando.style.display = "none";
-    resultado.style.display = "flex";
-
-    var entrada = document.querySelector("#texto-principal");
-    var mensagem = entrada.value;
-
-    mensagem = mensagem.replaceAll("ai", "a");
-    mensagem = mensagem.replaceAll("enter", "e");
-    mensagem = mensagem.replaceAll("imes", "i");
-    mensagem = mensagem.replaceAll("ober", "o");
-    mensagem = mensagem.replaceAll("ufat", "u")
-
-    saida.value = mensagem;
     
+    var input = document.querySelector('#texto-principal');
+    var mensagem = input.value;
+    var novaMensagem = "";
+
+    var listaCaractere = mensagem.split("");
+
+    for(var i = 0; i < listaCaractere.length; i++) {
+        if(listaCaractere[i] == "a") {
+            listaCaractere[i] = "ai";
+        }
+
+        else if(listaCaractere[i] == "e") {
+            listaCaractere[i] = "enter";
+        }
+
+        else if(listaCaractere[i] == "i") {
+            listaCaractere[i] = "imes";
+        }
+
+        else if(listaCaractere[i] == "o") {
+            listaCaractere[i] = "ober";
+        }
+        
+        else if(listaCaractere[i] == "u") {
+            listaCaractere[i] = "ufat";
+        }
+
+        novaMensagem = novaMensagem + listaCaractere[i];
+    }
+output.value = novaMensagem;
+console.log(novaMensagem)
 }
